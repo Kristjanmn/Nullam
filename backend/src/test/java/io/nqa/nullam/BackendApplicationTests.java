ed4b37ae4e26f7ec72e18d62676479d0128d7b9f
Kristjanmn <kristjanmn@hotmail.com>
2024-04-10 15:25:34 +0300

    Created backend module

---
+++ backend/src/test/java/io/nqa/nullam/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nqa.nullam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
