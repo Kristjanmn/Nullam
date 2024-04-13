3cd65c063005dfb298761d46dbd33a03c67f23dd
Kristjanmn <kristjanmn@hotmail.com>
2024-04-13 14:44:27 +0300

    Created ParticipantRepository

---
+++ backend/src/main/java/io/nqa/nullam/repository/ParticipantRepository.java
@@ -0,0 +1,9 @@
+package io.nqa.nullam.repository;
+
+import io.nqa.nullam.entity.Participant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+}
