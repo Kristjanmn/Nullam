d9d933adf174205471107e674258194818949512
Kristjanmn <kristjanmn@hotmail.com>
2024-04-15 01:07:34 +0300

    Update EventService with basic functionality

--- backend/src/main/java/io/nqa/nullam/service/EventService.java
+++ backend/src/main/java/io/nqa/nullam/service/EventService.java
@@ -1,7 +1,60 @@
 package io.nqa.nullam.service;
 
+import io.nqa.nullam.entity.Event;
+import io.nqa.nullam.model.EventDTO;
+import io.nqa.nullam.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class EventService implements IEventService {
+    private final EventRepository eventRepository;
+    @Override
+    public List<Event> getEvents() {
+        return this.eventRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    @Override
+    public Event getEventById(long id) {
+        return this.eventRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public EventDTO eventToDto(Event event) {
+        return new ModelMapper().map(event, EventDTO.class);
+    }
+
+    @Override
+    public Event dtoToEvent(EventDTO eventDTO) {
+        return new ModelMapper().map(eventDTO, Event.class);
+    }
+
+    @Override
+    public List<EventDTO> getEventDtos() {
+        List<EventDTO> dtos = new ArrayList<>();
+        for (Event event : this.getEvents()) {
+            if (event != null)
+                dtos.add(this.eventToDto(event));
+        }
+        return dtos;
+    }
+
+    @Override
+    public EventDTO getEventDtoById(long id) {
+        Event event = this.getEventById(id);
+        if (event == null)
+            return null;
+        return this.eventToDto(event);
+    }
+
+    @Override
+    public Event saveEvent(Event event) {
+        return this.eventRepository.save(event);
+    }
 }
--- backend/src/main/java/io/nqa/nullam/service/IEventService.java
+++ backend/src/main/java/io/nqa/nullam/service/IEventService.java
@@ -1,4 +1,16 @@
 package io.nqa.nullam.service;
 
+import io.nqa.nullam.entity.Event;
+import io.nqa.nullam.model.EventDTO;
+
+import java.util.List;
+
 public interface IEventService {
+    List<Event> getEvents();
+    Event getEventById(long id);
+    EventDTO eventToDto(Event event);
+    Event dtoToEvent(EventDTO eventDTO);
+    List<EventDTO> getEventDtos();
+    EventDTO getEventDtoById(long id);
+    Event saveEvent(Event event);
 }
