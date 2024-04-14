adf9da899eb36469909d43bac14f17b233cf365b
Kristjanmn <kristjanmn@hotmail.com>
2024-04-15 00:15:48 +0300

    Created PersonRepository and OrganizationRepository

---
+++ backend/src/main/java/io/nqa/nullam/repository/OrganizationRepository.java
@@ -0,0 +1,9 @@
+package io.nqa.nullam.repository;
+
+import io.nqa.nullam.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
---
+++ backend/src/main/java/io/nqa/nullam/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package io.nqa.nullam.repository;
+
+import io.nqa.nullam.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
