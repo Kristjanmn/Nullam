06d2cd0f0177b66febfc5aaaf4583df709fac213
Kristjanmn <kristjanmn@hotmail.com>
2024-04-16 18:09:09 +0300

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
 
 type HomePageProps = {};
 
 export const HomePage: React.FC<HomePageProps> = (props: HomePageProps) => {
     const { t } = useTranslation()
 
+    const infoPanel = () => {
+        return (
+            <>
+                <Splitter className="info-panel">
+                    <SplitterPanel>
+                        <div className="info-panel-text">
+                            {t('home.info-text')}
+                        </div>
+                    </SplitterPanel>
+                    <SplitterPanel>
+                        <img className="info-panel-image" src={`//${window?.top?.location.host}/pilt.jpg`} alt="image" />
+                    </SplitterPanel>
+                </Splitter>
+            </>
+        );
+    };
+
     return (
-        <>homepage</>
+        <>
+            {infoPanel()}
+        </>
     );
 };
