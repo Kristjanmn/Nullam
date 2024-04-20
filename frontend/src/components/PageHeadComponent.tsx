9269689452c5bc2944d9d6585dc50a3e27ae5b98
Kristjanmn <kristjanmn@hotmail.com>
2024-04-20 19:20:31 +0300

    Create PageHeadComponent.tsx

---
+++ frontend/src/components/PageHeadComponent.tsx
@@ -0,0 +1,21 @@
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+import React, { FC } from 'react';
+
+type PageHeadComponentProps = {
+    text: string;
+}
+
+export const PageHeadComponent: FC<PageHeadComponentProps> = (props: PageHeadComponentProps) => {
+    return (
+        <>
+            <Splitter className="page-head">
+                <SplitterPanel size={25}>
+                    <div className="page-head-text">{props.text}</div>
+                </SplitterPanel>
+                <SplitterPanel size={75}><img className="page-head-image" src={`//${window?.top?.location.host}/libled.jpg`} alt="remove"/></SplitterPanel>
+            </Splitter>
+        </>
+    );
+};
+
+export default PageHeadComponent;
