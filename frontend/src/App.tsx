00c61232774c5140444552db0441ce0bf6f18cf7
Kristjanmn <kristjanmn@hotmail.com>
2024-04-20 09:26:34 +0300

    Created Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import './App.css';
 import { RouterProvider } from './components/RouterProvider';
 import GlobalStyles from './GlobalStyles';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <div className="content">
                 <Header />
                 <RouterProvider />
+                <Footer />
             </div>
         </div>
     );
