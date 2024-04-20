dde41d4b0caba3f2c83d5b8e504d969f4381ef62
Kristjanmn <kristjanmn@hotmail.com>
2024-04-20 14:20:59 +0300

    Update GlobalStyles

--- frontend/src/GlobalStyles.tsx
+++ frontend/src/GlobalStyles.tsx
@@ -67,13 +67,56 @@ const GlobalStyles = createGlobalStyle`
         margin-bottom: 2rem;
         border: 0px;
     }
-    .home-events {}
+    .events-panel-left {
+        margin-right: 1rem;
+    }
+    .events-panel-right {
+        margin-left: 1rem;
+    }
+    .home-events {
+        width: 100%;
+    }
     .home-events-header {
         background-color: #005aa1;
         color: white;
+        padding: 1rem;
     }
-    .home-events-content {}
-    .home-events-item {}
+    .home-events-content {
+        background-color: white;
+    }
+    .home-events-item {
+        display: block;
+    }
+    .home-events-item-participants {
+        font-size: small;
+        cursor: pointer;
+    }
+    .home-events-item-remove {
+        width: 20px;
+        margin-left: 1rem;
+        cursor: pointer;
+    }
+    .home-events-table {
+    }
+    .home-events-table-header {
+        padding: 10px;
+    }
+    .home-events-table-column {
+        border: none;
+        padding: 10px;
+    }
+    .home-events-table-column:hover {
+        border: none;
+        padding: 10px;
+    }
+    .home-events-new-event-btn {
+        margin-right: 100%;
+        padding: 1rem;
+        cursor: pointer;
+        font-weight: bold;
+        font-size: 12px;
+        display: inline;
+    }
     
     // FOOTER
     .footer {
