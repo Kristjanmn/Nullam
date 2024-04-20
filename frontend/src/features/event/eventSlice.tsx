52f4743ba4db36084a836e8c9053750a1b3684ee
Kristjanmn <kristjanmn@hotmail.com>
2024-04-20 09:27:24 +0300

    Update eventSlice

--- frontend/src/features/event/eventSlice.tsx
+++ frontend/src/features/event/eventSlice.tsx
@@ -1,4 +1,5 @@
 import { Participant } from '../participant/participantSlice';
+import { createSlice } from '@reduxjs/toolkit';
 
 export type Event = {
     id: number;
@@ -8,3 +9,15 @@ export type Event = {
     additionalInfo: string;
     participants: Participant[];
 };
+
+export type EventState = {}
+
+const initialState: EventState = {};
+
+const eventSlice = createSlice({
+    name: 'event',
+    initialState,
+    reducers: {}
+});
+
+export default eventSlice.reducer;
