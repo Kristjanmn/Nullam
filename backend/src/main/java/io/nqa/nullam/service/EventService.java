f4e7c86db33390b25bc4abd10d229e45f9c8deb7
Kristjanmn <kristjanmn@hotmail.com>
2024-04-22 02:07:41 +0300

    Update EventService

--- backend/src/main/java/io/nqa/nullam/service/EventService.java
+++ backend/src/main/java/io/nqa/nullam/service/EventService.java
@@ -1,13 +1,14 @@
 package io.nqa.nullam.service;
 
 import io.nqa.nullam.entity.Event;
+import io.nqa.nullam.entity.Participant;
 import io.nqa.nullam.model.EventDTO;
 import io.nqa.nullam.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EventService implements IEventService {
     private final EventRepository eventRepository;
+    private final IParticipantService participantService;
     private final ModelMapper modelMapper = new ModelMapper();
 
     @Override
     public List<Event> getEvents() {
-        return this.eventRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        return this.eventRepository.findAll();
+    }
+
+    @Override
+    public List<Event> getUpcomingEvents() {
+        return this.eventRepository.findAllByDateTimeIsAfterOrderByDateTimeAsc(LocalDateTime.now()).orElse(null);
+    }
+
+    @Override
+    public List<Event> getPastEvents() {
+        return this.eventRepository.findAllByDateTimeIsBeforeOrderByDateTimeDesc(LocalDateTime.now()).orElse(null);
     }
 
     @Override
@@ -48,6 +60,26 @@ public class EventService implements IEventService {
     }
 
     @Override
+    public List<EventDTO> getUpcomingEventDtos() {
+        List<EventDTO> dtos = new ArrayList<>();
+        for (Event event : this.getUpcomingEvents()) {
+            if (event != null)
+                dtos.add(this.eventToDto(event));
+        }
+        return dtos;
+    }
+
+    @Override
+    public List<EventDTO> getPastEventDtos() {
+        List<EventDTO> dtos = new ArrayList<>();
+        for (Event event : this.getPastEvents()) {
+            if (event != null)
+                dtos.add(this.eventToDto(event));
+        }
+        return dtos;
+    }
+
+    @Override
     public EventDTO getEventDtoById(long id) {
         Event event = this.getEventById(id);
         if (event == null)
@@ -59,6 +91,19 @@ public class EventService implements IEventService {
     public Event saveEvent(Event event) {
         if (event.getParticipants() == null)
             event.setParticipants(new ArrayList<>());
+        List<Participant> participants = new ArrayList<>();
+        for (int i = 0; i < event.getParticipants().size(); i++) {
+            participants.add(this.participantService.saveParticipant(event.getParticipants().get(i)));
+        }
+        event.setParticipants(participants);
         return this.eventRepository.save(event);
+    }
+
+    @Override
+    public void deleteEvent(long id) {
+        Event event = this.getEventById(id);
+        event.setParticipants(null);
+        event = this.saveEvent(event);
+        this.eventRepository.delete(event);
     }
 }
--- backend/src/main/java/io/nqa/nullam/service/IEventService.java
+++ backend/src/main/java/io/nqa/nullam/service/IEventService.java
@@ -7,10 +7,15 @@ import java.util.List;
 
 public interface IEventService {
     List<Event> getEvents();
+    List<Event> getUpcomingEvents();
+    List<Event> getPastEvents();
     Event getEventById(long id);
     EventDTO eventToDto(Event event);
     Event dtoToEvent(EventDTO eventDTO);
     List<EventDTO> getEventDtos();
+    List<EventDTO> getUpcomingEventDtos();
+    List<EventDTO> getPastEventDtos();
     EventDTO getEventDtoById(long id);
     Event saveEvent(Event event);
+    void deleteEvent(long id);
 }
