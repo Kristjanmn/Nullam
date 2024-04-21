5d5078d6ca8b751718ab30fcf6d485c2ac5cb420
Kristjanmn <kristjanmn@hotmail.com>
2024-04-21 10:36:39 +0300

    Update RouterProvider.tsx

--- frontend/src/components/RouterProvider.tsx
+++ frontend/src/components/RouterProvider.tsx
@@ -1,7 +1,9 @@
 import { ElementType, JSX } from 'react';
-import { HomePage } from '../pages/HomePage';
+import { HomePage } from '../pages/Home/HomePage';
 import { Route, Routes } from 'react-router-dom';
-import { NewEventPage } from '../pages/NewEventPage';
+import { NewEventPage } from '../pages/Event/NewEventPage';
+import { EventDetailsPage } from '../pages/Event/EventDetailsPage';
+import { ParticipantDetailsPage } from '../pages/Event/ParticipantDetailsPage';
 
 export type RouteType = {
     path: string,
@@ -27,6 +29,8 @@ export const RouterProvider = (): JSX.Element => {
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/newevent' element={<NewEventPage />} />
+            <Route path="/event/:id" element={<EventDetailsPage />} />
+            <Route path="/participant/:id" element={<ParticipantDetailsPage />} />
         </Routes>
     );
 };
