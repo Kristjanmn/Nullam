3a97ba047ab2b98b2b54c28a44072a05d41e8110
Kristjanmn <kristjanmn@hotmail.com>
2024-04-22 02:07:23 +0300

    Update EventRepository

--- backend/src/main/java/io/nqa/nullam/repository/EventRepository.java
+++ backend/src/main/java/io/nqa/nullam/repository/EventRepository.java
@@ -2,6 +2,14 @@ package io.nqa.nullam.repository;
 
 import io.nqa.nullam.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Optional<List<Event>> findAllByDateTimeIsBeforeOrderByDateTimeDesc(LocalDateTime dateTime);
+    Optional<List<Event>> findAllByDateTimeIsAfterOrderByDateTimeAsc(LocalDateTime dateTime);
 }
