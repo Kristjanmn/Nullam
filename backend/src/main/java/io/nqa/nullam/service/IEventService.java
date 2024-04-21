2f8c4364925f7283f21de0d669ea36233ee6e411
Kristjanmn <kristjanmn@hotmail.com>
2024-04-22 02:36:33 +0300

    Comment EventService and ParticipantService

--- backend/src/main/java/io/nqa/nullam/service/IEventService.java
+++ backend/src/main/java/io/nqa/nullam/service/IEventService.java
@@ -6,16 +6,94 @@ import io.nqa.nullam.model.EventDTO;
 import java.util.List;
 
 public interface IEventService {
+    /**
+     * Get all events.
+     *
+     * @return All events from database
+     */
     List<Event> getEvents();
+
+    /**
+     * Get all future events.
+     *
+     * @return List of upcoming events
+     */
     List<Event> getUpcomingEvents();
+
+    /**
+     * Get all past events.
+     *
+     * @return List of past events
+     */
     List<Event> getPastEvents();
+
+    /**
+     * Get one event by its database id;
+     *
+     * @param id Database ID
+     * @return Event object
+     */
     Event getEventById(long id);
+
+    /**
+     * Convert Event -> EventDTO
+     * Used before sending data to client.
+     *
+     * @param event Object to convert
+     * @return Converted Event
+     */
     EventDTO eventToDto(Event event);
+
+    /**
+     * Convert EventDTO -> Event
+     * Used before saving Event
+     *
+     * @param eventDTO Object to convert
+     * @return Converted Event
+     */
     Event dtoToEvent(EventDTO eventDTO);
+
+    /**
+     * Convert all Events to DTOs.
+     *
+     * @return List of DTOs
+     */
     List<EventDTO> getEventDtos();
+
+    /**
+     * Get all future events as DTOs.
+     *
+     * @return List of DTOs
+     */
     List<EventDTO> getUpcomingEventDtos();
+
+    /**
+     * Get all past events as DTOs
+     *
+     * @return List of DTOs
+     */
     List<EventDTO> getPastEventDtos();
+
+    /**
+     * Get Event as DTO by database ID;
+     *
+     * @param id Database ID
+     * @return Event DTO
+     */
     EventDTO getEventDtoById(long id);
+
+    /**
+     * Save event.
+     *
+     * @param event Object to save
+     * @return Saved Event object
+     */
     Event saveEvent(Event event);
+
+    /**
+     * Delete event.
+     *
+     * @param id Database ID
+     */
     void deleteEvent(long id);
 }
--- backend/src/main/java/io/nqa/nullam/service/IParticipantService.java
+++ backend/src/main/java/io/nqa/nullam/service/IParticipantService.java
@@ -6,11 +6,59 @@ import io.nqa.nullam.model.ParticipantDTO;
 import java.util.List;
 
 public interface IParticipantService {
+    /**
+     * Get all participants from database.
+     *
+     * @return All participants from database
+     */
     List<Participant> getParticipants();
+
+    /**
+     * Get participant by database ID.
+     *
+     * @param id Database ID
+     * @return Participant object
+     */
     Participant getParticipantById(long id);
+
+    /**
+     * Convert Participant -> ParticipantDTO
+     * Used before sending data to client.
+     *
+     * @param participant Object to convert
+     * @return Converted object
+     */
     ParticipantDTO participantToDto(Participant participant);
+
+    /**
+     * Convert ParticipantDTO -> Participant
+     * Used before saving data.
+     *
+     * @param participantDTO Object to convert
+     * @return Converted object
+     */
     Participant dtoToParticipant(ParticipantDTO participantDTO);
+
+    /**
+     * Get all participants as DTOs.
+     *
+     * @return List of Participants as DTOs
+     */
     List<ParticipantDTO> getParticipantDtos();
+
+    /**
+     * Get participant by database ID as DTO.
+     *
+     * @param id Database ID
+     * @return Participant as DTO
+     */
     ParticipantDTO getParticipantDtoById(long id);
+
+    /**
+     * Save participant to database.
+     *
+     * @param participant Object to save
+     * @return Saved Participant object
+     */
     Participant saveParticipant(Participant participant);
 }
